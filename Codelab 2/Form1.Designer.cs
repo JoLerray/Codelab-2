b3b6de6e081071c6c88412844cbda4cf595bff6b
EshDel <EshDel@DESKTOP-LUSFDUQ>
2022-03-15 19:07:22 +0700

    Добавьте файлы проекта.

---
+++ Codelab 2/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace Codelab_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(101)))), ((int)(((byte)(114)))));
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.menuStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.Flow;
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.menuStrip1.Size = new System.Drawing.Size(1920, 23);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(85)))));
+            this.ClientSize = new System.Drawing.Size(1920, 1080);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+    }
+}
+
---
+++ Codelab 2/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Windows;
+namespace Codelab_2
+{
+    public partial class Form1 : Form
+    {
+
+
+        List<Item> itemsVal = new List<Item>();
+        private class Item
+        {
+            public int hierarchy; public string text; public int status; public string name;
+            public Item(int hierarchy, string text, int status, string name)
+            {
+                this.hierarchy = hierarchy; this.text = text; this.status = status; this.name = name; 
+            }
+
+        }
+        private ToolStripMenuItem createMenuItem(int id,int hierarchy = 0)
+        {
+            ToolStripMenuItem _item = new ToolStripMenuItem(itemsVal[id].text);
+            _item.ForeColor = Color.FromArgb(35,47,52);
+            _item.BackColor = Color.FromArgb(74, 101, 114);
+            if (hierarchy == itemsVal[id].hierarchy)
+            {
+                switch (itemsVal[id].status)
+                {
+                    case 0:
+                        {
+                            _item.Visible = true;
+                            _item.Enabled = true;
+                            break;
+                        }
+                    case 1:
+                        {
+                            _item.Visible = true;
+                            _item.Enabled = false;
+                            break;
+                        }
+                    case 2:
+                        {
+                            _item.Visible = false;
+                            _item.Enabled = false;
+                            break;
+                        }
+                }
+
+                if (itemsVal[id].name != "") return _item;
+                if (itemsVal[id].name == "")
+                {
+                    hierarchy = itemsVal[id].hierarchy + 1;
+
+                    while (hierarchy <= itemsVal[id + 1].hierarchy)
+                    {
+                        _item.DropDownItems.Add(createMenuItem(id + 1, hierarchy));
+                        id++;
+                        if (id + 1 >= itemsVal.Count) break;
+                    }
+
+                }
+                return _item;
+            }
+            ToolStripMenuItem __item = new ToolStripMenuItem();
+            __item.Visible = false;
+            __item.Enabled = false;
+            return __item;
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            string path = (@"C:\Users\EshDel\Desktop\Design.txt");
+            StreamReader file = new StreamReader(path);
+            ToolStripMenuItem fileItem = new ToolStripMenuItem("file");
+            List<string> DesignItems = new List<string>();
+            while (true)
+            {
+                try
+                {
+                    var val = file.ReadLine();
+                    if (val == null) break;
+                    DesignItems.Add(val);
+
+                }
+                catch
+                {
+                    break;
+                }
+            }
+            foreach (var item in DesignItems)
+            {
+                var valueArray =  item.Split();
+                int hierarchy = System.Convert.ToInt32(valueArray[0]);
+                string text = valueArray[1]; 
+                int status =  System.Convert.ToInt32(valueArray[2]);
+                string name = "";
+                if (valueArray.Length == 4) name = (valueArray[3]);
+                itemsVal.Add(new Item(hierarchy, text, status, name));
+
+            }
+            for(int i = 0; i < itemsVal.Count; i++)
+            {
+                if (itemsVal[i].hierarchy == 0) this.menuStrip1.Items.Add(createMenuItem(i));
+            }
+            menuStrip1.Renderer = new ToolStripProfessionalRenderer(new Cols());
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
